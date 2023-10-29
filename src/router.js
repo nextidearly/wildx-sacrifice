69fcbcddf0a3f74df270176f8ecb0536d4e7c1df
nextidearly <talenteddev0125@gmail.com>
2023-10-28 21:54:14 -0700

    sac

--- src/router.js
+++ src/router.js
@@ -2,11 +2,6 @@ import React, { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 const ComingSoon = lazy(() => import("pages/ComingSoon"));
-const Home = lazy(() => import("pages/Home"));
-const Swap = lazy(() => import("pages/Swap"));
-const Liquidity = lazy(() => import("pages/Liquidity"));
-const NotFound = lazy(() => import("pages/Notfound"));
-const Farms = lazy(() => import("pages/Farms"));
 
 const router = createBrowserRouter([
   {
